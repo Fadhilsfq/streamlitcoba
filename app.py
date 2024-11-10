a91b1236280b883ac086caf16d9ab0d05b2a01c6
Fadhilsfq <145995449+Fadhilsfq@users.noreply.github.com>
2024-11-11 00:24:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Membaca dataset
-df = pd.read_csv('/content/drive/MyDrive/Colab Notebooks/Dataset/all_data.csv')
+df = pd.read_csv('all_datanew.csv')
 
 # Menampilkan judul
 st.title('Dashboard Sederhana ceunah')
