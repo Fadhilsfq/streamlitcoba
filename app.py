b7671efed16b59120e8b4a46b7145593335dce49
Fadhilsfq <145995449+Fadhilsfq@users.noreply.github.com>
2024-11-11 01:50:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Membaca dataset
-df = pd.read_csv('all_datanew.csv')
+df = pd.read_csv('all_datarev.csv')
 
 # Menampilkan judul
 st.title('Dashboard Sederhana ceunah')
